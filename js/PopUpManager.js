b2f9fe15010c0da0d2ca7f2d8ebf736ad298561b
Marcin Bazydlo <marcin.bazydlo@gmail.com>
2013-03-14 19:49:31 +0000

    Cleaning Up

---
+++ js/PopUpManager.js
@@ -0,0 +1,54 @@
+function PopUpManager(sigInst, selector){
+    var popUp;
+
+    function attributesToString(attr) {
+        var res ="";
+        for (var k in attr){
+            if (attr.hasOwnProperty(k)) {
+                res += k + ": " + attr[k]+"<br/>";
+            }
+        }
+        return res;
+    }
+
+    function showNodeInfo(event) {
+        popUp && popUp.remove();
+
+        var node;
+        sigInst.iterNodes(function(n){
+            node = n;
+        },[event.content[0]]);
+
+        popUp = $(
+            "<div>"+attributesToString(node.attr.reservation)+"</div>"
+        ).attr(
+                'id',
+                'node-info'+sigInst.getID()
+            ).css({
+                'display': 'inline-block',
+                'border-radius': 3,
+                'padding': 5,
+                'background': '#fff',
+                'color': '#000',
+                'box-shadow': '0 0 4px #666',
+                'position': 'absolute',
+                'left': node.displayX,
+                'top': node.displayY+15
+            });
+
+        $('ul',popUp).css('margin','0 0 0 20px');
+
+        $(selector).append(popUp);
+    }
+
+    function hideNodeInfo(event) {
+        popUp && popUp.remove();
+        popUp = false;
+    }
+
+    return {
+        AddPopUp: function(){
+            sigInst.bind('overnodes',showNodeInfo).bind('outnodes',hideNodeInfo).draw();
+        }
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
     var sigRoot = document.getElementById('graph');
     var sigInst = sigma.init(sigRoot);
-    //hw(sigInst);
-    AddPopUp(sigInst);
+    new PopUpManager(sigInst,'#graph').AddPopUp();
     var data = DataSource().GetData();
     for(var reservationId in data.reservations)
     {
@@ -27,70 +26,6 @@ $(document).ready(function(){
     });
     sigInst.startForceAtlas2();
 })
-
-
-function AddPopUp(sigInst){
-    var popUp;
-
-    function attributesToString(attr) {
-        var res ="";
-        for (var k in attr){
-            if (attr.hasOwnProperty(k)) {
-                res += k + ": " + attr[k]+"<br/>";
-            }
-        }
-        return res;
-    }
-
-    function showNodeInfo(event) {
-    popUp && popUp.remove();
-
-    var node;
-    sigInst.iterNodes(function(n){
-        node = n;
-    },[event.content[0]]);
-
-    popUp = $(
-        "<div>"+attributesToString(node.attr.reservation)+"</div>"
-        ).attr(
-            'id',
-            'node-info'+sigInst.getID()
-        ).css({
-            'display': 'inline-block',
-            'border-radius': 3,
-            'padding': 5,
-            'background': '#fff',
-            'color': '#000',
-            'box-shadow': '0 0 4px #666',
-            'position': 'absolute',
-            'left': node.displayX,
-            'top': node.displayY+15
-        });
-
-    $('ul',popUp).css('margin','0 0 0 20px');
-
-    $('#graph').append(popUp);
-    }
-
-    function hideNodeInfo(event) {
-        popUp && popUp.remove();
-        popUp = false;
-    }
-
-    sigInst.bind('overnodes',showNodeInfo).bind('outnodes',hideNodeInfo).draw();
-}
-
-function hw(sigInst){
-    sigInst.addNode('hello',{
-        label: 'Hello',
-        color: '#ff0000',
-        x:0.01
-    }).addNode('world',{
-            label: 'World !',
-            color: '#00ff00',
-            size: 1
-        }).addEdge('hello_world','hello','world').draw();
-}
 
 function partnerIdToColor(partnerId){
     if (partnerId == 1)
