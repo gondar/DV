8b4e499201d6d5be1d7ee82ce3e8ea2becfa03cc
Marcin Bazydlo <marcin.bazydlo@gmail.com>
2013-04-03 10:58:03 +0100

    Added real fullscreen

--- js/Fullscreen.js
+++ js/Fullscreen.js
@@ -1,5 +1,32 @@
 function Fullscreen(){
     var fullscreen = false;
+
+    function requestFullScreen(element) {
+        // Supports most browsers and their versions.
+        var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+        if (requestMethod) { // Native full screen.
+            requestMethod.call(element);
+        } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+            var wscript = new ActiveXObject("WScript.Shell");
+            if (wscript !== null) {
+                wscript.SendKeys("{F11}");
+            }
+        }
+    }
+
+    function closeFullScreen(){
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+        else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        }
+        else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }
+    }
+
     return {
         On: function(){
             if (fullscreen) {
@@ -13,6 +40,7 @@ function Fullscreen(){
             $('#navigationBar').removeClass("navbar-fixed-top");
             $('#navigationBar').addClass("navbar-fixed-bottom");
             $("body").css("background-color","#222222");
+            requestFullScreen(document.body);
         },
         Off: function(){
             if (!fullscreen) {
@@ -27,6 +55,7 @@ function Fullscreen(){
             $('#navigationBar').addClass("navbar-fixed-top");
             $('#navigationBar').removeClass("navbar-fixed-bottom");
             $("body").css("background-color","#ffffff");
+            closeFullScreen();
         }
 
     }
